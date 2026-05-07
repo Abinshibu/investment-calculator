d6e1b588ce1f2509803bbeb12e1a76912b9cebd1
Abinshibu <abin.s@strat-agile.com>
2026-05-07 14:28:14 +0530

    final commit

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { useState } from 'react';
 import Header from './components/Header';
 import UserInput from './components/userInput';
 import Result from './components/Result';
+
 function App() {
   const[userInput, setUserInput] = useState({
     initialInvestment: 12000,
@@ -10,6 +11,8 @@ function App() {
     expectedReturnRate: 7,
     investmentPeriod: 10
   });
+
+  const inputIsValid = Object.values(userInput).every(value => value > 0);
 
   function handleChange(inputIdentifier, newValue) {
     setUserInput((prevUserInput) => {
@@ -24,7 +27,8 @@ function App() {
     <>
       <Header />
       <UserInput userInput={userInput} onChange={handleChange} />
-      <Result input={userInput} />
+      {!inputIsValid && <p id="validation-message" className='center'>Please enter valid input values (greater than zero).</p>}
+      {inputIsValid && <Result input={userInput}  />}
 
     </>
     
